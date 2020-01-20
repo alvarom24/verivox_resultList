798760445c96357da7c4ef663cdf6ad51e14ddb7
Matias Montes <matiasmontes@Matiass-MacBook-Pro.local>
2020-01-20 13:57:48 -0500

    Filters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,10 @@ import { resultModel } from "./shared/models/result.model";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  styleUrls: ["./app.component.scss"],
+  host: {
+    "(window:resize)": "onResize($event)"
+  }
 })
 export class AppComponent implements OnInit {
   public title: string;
@@ -20,9 +23,15 @@ export class AppComponent implements OnInit {
     this.isTablet = /ipad/.test(navigator.userAgent.toLowerCase());
   }
 
-  public async ngOnInit() {
-    this.resultList = await this.resultListService.getDataList();
-    console.log(this.resultList);
+  public async ngOnInit(): Promise<void> {
+    this.fetchResults();
+  }
+
+  public onResize(event): void {
+    this.isMobile = /android|webos|iphone|ipod|blackberry|windows phone/.test(
+      navigator.userAgent.toLowerCase()
+    );
+    this.isTablet = /ipad/.test(navigator.userAgent.toLowerCase());
   }
 
   public getRedirectLink(item: resultModel): string {
@@ -33,7 +42,38 @@ export class AppComponent implements OnInit {
     if (!this.isMobile && item.signup.desktop) {
       return item.signup.desktop.url;
     }
-
     return "";
+  }
+
+  public sortBy(criteria: string): void {
+    switch (criteria) {
+      case "upload":
+        this.resultList.sort((a, b) =>
+          a.contractTerm.uploadSpeed.amount > b.contractTerm.uploadSpeed.amount
+            ? 1
+            : -1
+        );
+        break;
+      case "download":
+        this.resultList.sort((a, b) =>
+          a.contractTerm.downloadSpeed.amount >
+          b.contractTerm.downloadSpeed.amount
+            ? 1
+            : -1
+        );
+        break;
+      case "price":
+        this.resultList.sort((a, b) =>
+          a.cost.effectiveCost.amount > b.cost.effectiveCost.amount ? 1 : -1
+        );
+        break;
+      default:
+        this.fetchResults();
+        break;
+    }
+  }
+
+  private async fetchResults(): Promise<void> {
+    this.resultList = await this.resultListService.getDataList();
   }
 }
